897d7aca289235ca54c65a621ad92c2e8175d999
Ben Hickson <bzhickson@gmail.com>
2020-02-08 18:44:20 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
-  # your code here
+  output_array = []
+  counter = 0
+  while array[counter] do     # continues looping as long as there are items, because if you request an array position that does not exist, nil will be returned, which is falsey
+    
+    output_array.push(array[counter] ** 2)
+    
+    counter += 1
+  end
+  
+  return output_array
+  
 end
